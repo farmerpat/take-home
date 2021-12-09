17731a9a4a92953e93b512866589d095393a55a3
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2021-12-09 17:39:37 -0500

    init skeleton

---
+++ README.md
@@ -0,0 +1,20 @@
+# take-home
+
+The take-home exercise.
+
+## Prerequisites
+
+You will need [Leiningen][1] 2.0 or above installed.
+
+[1]: https://github.com/technomancy/leiningen
+
+## Running
+
+To start a web server for the application, run:
+
+```bash
+$ lein run 
+```
+
+## TODO
+Write it!
---
+++ src/clj/take_home/routes/home.clj
@@ -0,0 +1,21 @@
+(ns take-home.routes.home
+  (:require
+   [take-home.layout :as layout]
+   [clojure.java.io :as io]
+   [take-home.middleware :as middleware]
+   [ring.util.response]
+   [ring.util.http-response :as response]))
+
+(defn home-page [request]
+  (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+
+(defn about-page [request]
+  (layout/render request "about.html"))
+
+(defn home-routes []
+  [ "" 
+   {:middleware [middleware/wrap-csrf
+                 middleware/wrap-formats]}
+   ["/" {:get home-page}]
+   ["/about" {:get about-page}]])
+
